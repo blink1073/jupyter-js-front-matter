cd434c0ac832bd628610abf6f96e1c13436db005
Steven Silvester <steven.silvester@ieee.org>
2015-12-09 14:08:06 -0600

    Update to use stealjs for testing

--- test/karma-cov.conf.js
+++ test/karma-cov.conf.js
@@ -1,19 +1,22 @@
 module.exports = function (config) {
   config.set({
+    basePath: '..',
     browsers: ['Firefox'],
-    frameworks: ['browserify', 'mocha'],
+    frameworks: ['mocha'],
     reporters: ['mocha', 'coverage'],
-    preprocessors: { 'build/*.js': ['browserify'] },
-    browserify: {
-      debug: true,
-      transform: ['browserify-css', 'browserify-istanbul']
-    },
-    files: ['build/*.js'],
+    files: [
+      { pattern: 'lib/*.*', included: false },
+      { pattern: 'package.json', included: false },
+      { pattern: 'node_modules/**/*.*', included: false },
+      { pattern: 'test/build/index.*', included: false },
+      'node_modules/steal/steal.js',
+      'test/karma.bootstrap.js'
+    ],
     coverageReporter: {
       reporters : [
         { 'type': 'text' },
-        { 'type': 'lcov', dir: 'coverage' },
-        { 'type': 'html', dir: 'coverage' }
+        { 'type': 'lcov', dir: 'test/coverage' },
+        { 'type': 'html', dir: 'test/coverage' }
       ]
     },
     port: 9876,
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,10 +1,16 @@
 module.exports = function (config) {
   config.set({
-    frameworks: ['browserify', 'mocha'],
+    basePath: '..',
+    frameworks: ['mocha'],
     reporters: ['mocha'],
-    preprocessors: { 'build/*.js': ['browserify'] },
-    browserify: { debug: true, transform: ['browserify-css'] },
-    files: ['build/*.js'],
+    files: [
+      { pattern: 'lib/*.*', included: false },
+      { pattern: 'package.json', included: false },
+      { pattern: 'node_modules/**/*.*', included: false },
+      { pattern: 'test/build/index.*', included: false },
+      'node_modules/steal/steal.js',
+      'test/karma.bootstrap.js'
+    ],
     port: 9876,
     colors: true,
     singleRun: true,
